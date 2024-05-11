b1291110dbf4facc4c80fab0b2267387608a9c1d
jebarsonponraj <jebarsonponraj@gmail.com>
2024-05-11 12:40:25 +0530

    Fixed some UI bugs

--- src/components/ProgressComponent.jsx
+++ src/components/ProgressComponent.jsx
@@ -13,8 +13,10 @@ const ProgressComponent = () => {
     <section className="col-span-7 w-full">
         <div className='w-full grid grid-cols-3'>
             {progressTabList.map((list) => (
-                <div key={list.id} className={`col-span-1 py-2 w-full flex justify-center items-center space-x-4 border shadow-md ${selectedTab === list.id ? 'bg-white text-violet-800' : 'bg-slate-100 hover:bg-slate-50'} ease-in-out duration-200 cursor-pointer`} onClick={() => handleSelectedTabChange(list.id)}>
+                <div key={list.id} className={`col-span-1 py-2 w-full flex justify-center items-center max-sm:text-xs space-x-4 border shadow-md ${selectedTab === list.id ? 'bg-white text-violet-800' : 'bg-slate-100 hover:bg-slate-50'} ease-in-out duration-200 cursor-pointer`} onClick={() => handleSelectedTabChange(list.id)}>
+                    <div className='max-sm:ml-2'>
                     <list.icon />
+                    </div>
                     <span>{list.label}</span>
                 </div>
             ))}
